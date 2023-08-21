1aa6ce8c9ec2bf0139f533fd8db1095cc17ad080
Vyacheslav Shakhov <slava.shaxov.98@mail.ru>
2023-08-21 12:16:39 +0300

    Create cards, scss modules

--- src/App.js
+++ src/App.js
@@ -4,36 +4,50 @@ import Card from './components/card/Card';
 
 import './app.scss';
 
+const arr = [
+    {imgUrl: './img/sneakers-1.jpg', title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999'},
+    {imgUrl: './img/sneakers-2.jpg', title: 'Мужские Кроссовки Nike Air Max 270', price: '13 999'},
+    {imgUrl: './img/sneakers-3.jpg', title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499'},
+    {imgUrl: './img/sneakers-4.jpg', title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999'},
+    {imgUrl: './img/sneakers-5.jpg', title: 'Мужские Кроссовки Under Armour Curry 8', price: '15 199'},
+    {imgUrl: './img/sneakers-6.jpg', title: 'Мужские Кроссовки Nike Kyrie 7', price: '11 299'},
+    {imgUrl: './img/sneakers-7.jpg', title: 'Мужские Кроссовки Jordan Air Jordan 11', price: '10 799'},
+    {imgUrl: './img/sneakers-8.jpg', title: 'Мужские Кроссовки Nike LeBron XVIII', price: '16 499'},
+    {imgUrl: './img/sneakers-9.jpg', title: 'Мужские Кроссовки Nike Lebron XVIII Low', price: '13 999'},
+    {imgUrl: './img/sneakers-10.jpg', title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499'},
+    {imgUrl: './img/sneakers-11.jpg', title: 'Кроссовки Puma X Aka Boku Future Rider', price: '8 999'},
+    {imgUrl: './img/sneakers-12.jpg', title: 'Мужские Кроссовки Nike Kyrie Flytrap IV', price: '11 299'},
+]
+
 function App() {
-  return (
-    <div className="wrapper">
-      <Drawer/>
-      <Header/>
-      <div className='content'>
-        <div className="content__header">
-          <h1>Все кроссовки</h1>
-          <div className="search__block">
-            <img src="./img/search.svg" 
-                 alt="search"
-                 className='seacrh__icon' />
-            <input className='search' 
-                  type="text" 
-                  name="search"
-                  placeholder='Поиск...'/>
-          </div>
+    return (
+        <div className="wrapper">
+            <Drawer/>
+            <Header/>
+            <div className='content'>
+                <div className="content__header">
+                    <h1>Все кроссовки</h1>
+                    <div className="search__block">
+                        <img src="./img/search.svg" 
+                            alt="search"
+                            className='seacrh__icon' />
+                        <input className='search' 
+                            type="text" 
+                            name="search"
+                            placeholder='Поиск...'/>
+                    </div>
+                </div>
+                <div className="sneakers">
+                    {arr.map((obj, i) => (
+                        <Card imgUrl={obj.imgUrl} 
+                              title={obj.title} 
+                              price={obj.price}
+                              key={i}/>
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className="sneakers">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App;
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,27 @@
-function Card() {
+import style from './Card.module.scss';
+
+function Card(props) {
     return (
-        <div className="card">
-            <div className="favorit">
-              <img width={32} height={32} src="./img/heart-unliked.svg" alt="unliked" />
+        <div className={style.card}>
+            <div className={style.favorit}>
+                <img width={32} height={32} src="./img/heart-unliked.svg" alt="unliked" />
             </div>
-            <div className="card__info">
-              <img className='card__img' src="./img/blazer.jpg" alt="sneaker" />
-              <p className='card__descr'>Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <div>
+                <img width={133}
+                     height={112}
+                     src={props.imgUrl} alt="sneaker" />
+                <p className={style.card__descr}>{props.title}</p>
             </div>
-            <div className='card__price-block'>
-              <div className='card-block'>
-                <span className='card__price'>Цена:</span>
-                <b className='card__rub'>12 999 руб.</b>
-              </div>
-              <button className='button'>
-                <img width={32} height={32} src="./img/plus.svg" alt="add to card" />
-              </button>
+            <div className={style.price}>
+                <div>
+                    <span>Цена:</span>
+                    <b>{props.price} руб.</b>
+                </div>
+                <button className='button'>
+                    <img width={32} height={32} src="./img/plus.svg" alt="add to card" />
+                </button>
             </div>
-          </div>
+        </div>
     )
 }
 
